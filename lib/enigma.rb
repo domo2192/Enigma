e15a984bc4ff10ba99a415b90bea51273f6e9d25
Dominic Padula <domopadula2192@gmail.com>
2021-01-14 18:12:04 -0700

    First few methods and tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,4 +5,29 @@ class Enigma
   def alphabet
     ("a".."z").to_a << " "
   end
+
+  def convert_date
+    Date.today.strftime("%m%d%y")
+  end
+
+  def key_numbers
+    5.times.map do |num|
+        rand(9)
+    end.join
+  end
+
+  def split_keys(digits)
+   digits.split('').each_cons(2).map do |pair|
+      pair.join.to_i
+    end
+  end
+
+  def create_offsets(date)
+    
+
+  # def encrypt(message, key = default_key, date = default_date)
+  #   {encryption: cipher,
+  #               key: key,
+  #               date: date}
+  # end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -15,4 +15,35 @@ class EnigmaTest < Minitest::Test
   def test_alphabet_contains_27_chars
     assert_equal 27, @enigma.alphabet.length
   end
+
+  def test_dates_can_be_converted
+    assert_equal "011421", @enigma.convert_date
+  end
+
+  def test_keys_can_be_generated
+    numbers = (0..9).to_a
+      @enigma.key_numbers.split("").each do |number|
+    assert_includes numbers, number.to_i
+    end
+
+  end
+
+  def test_split_digits_to_key
+    assert_equal [02 , 27, 71, 15], @enigma.split_keys('02715')
+  end
+
+  def test_encrypt_is_hash_taking_three_arguements
+    cipher = mock
+    key = mock
+    date = mock
+    expected = {encryption: cipher,
+                key: key,
+                date: date}
+    assert_equal expected, @enigma.encrypt('akjakj jfj')
+  end
+
+  def test_offsets
+    assert_equal [1, 0 , 2 , 5], @enigma.create_offsets(040895)
+
+  end
 end
