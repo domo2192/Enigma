3534c203936c294920983506087adf2f29bb3f31
Dominic Padula <domopadula2192@gmail.com>
2021-01-17 09:32:21 -0700

    Pull out enigma files into own classes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -22,8 +22,26 @@ class Enigma
     end
   end
 
-  def create_offsets(date)
-    
+  def create_offsets(convert_date)
+    hold = []
+    x = (convert_date.to_i)
+    y = x ** 2
+    a = y.to_s[-4..-1]
+    a.split('')
+    hold << a.split('').map(&:to_i)
+    hold.flatten
+  end
+
+  def create_values(arg_1, arg_2)
+    split = split_keys(arg_1)
+    offset = create_offsets(arg_2)
+    combine = [offset, split]
+    combine.transpose.map {|x| x.reduce(:+)}
+  end
+
+  def final_shifts(values)
+    hash = {A:values[0],B:values[1],C:values[2],D:values[3]}
+  end
 
   # def encrypt(message, key = default_key, date = default_date)
   #   {encryption: cipher,
