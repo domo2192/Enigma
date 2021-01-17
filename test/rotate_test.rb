0c3550fcdb7daa63d141e2d7388ff039f38111a9
Dominic Padula <domopadula2192@gmail.com>
2021-01-17 09:33:40 -0700

    Write tests for rotate words and rotate letters

---
+++ test/rotate_test.rb
@@ -0,0 +1,32 @@
+require './test/test_helper'
+require './lib/keygenerator'
+require './lib/enigma'
+require './lib/rotate'
+
+
+class KeyGeneratorTest < Minitest::Test
+
+  def setup
+    @generate_key = KeyGenerator.new
+    @enigma = Enigma.new
+    @rotate = Rotate.new
+  end
+
+  def test_it_exists
+    assert_instance_of Rotate, @rotate
+
+  end
+
+  def test_it_can_rotate_letters
+    assert_equal 'k', @rotate.rotate_letters("h", 3)
+  end
+
+  def test_alphabet_contains_27_chars
+    assert_equal 27, @rotate.alphabet.length
+  end
+
+  def test_it_can_rotate_words
+    expected = 'keder ohulw!'
+    assert_equal expected, @rotate.rotate_words('hello world!', [3, 27, 73, 20])
+  end
+end
