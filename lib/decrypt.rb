ffe2b744e87f1b2dd8f91bddac3680f0c55bd782
Dominic Padula <domopadula2192@gmail.com>
2021-01-18 16:16:20 -0700

    remove downcase method

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,7 +3,7 @@ require './lib/enigma'
 encrypted_text = File.open(ARGV[0])
 
 enigma = Enigma.new
-decoded = enigma.decrypt(encrypted_text.downcase, ARGV[2], ARGV[3])
+decoded = enigma.decrypt(encrypted_text, ARGV[2], ARGV[3])
 File.open(ARGV[1], 'w').write(decoded[:decryption])
 
 p "Created '#{ARGV[1]}' with the key #{decoded[:key]} and date #{decoded[:date]}"
