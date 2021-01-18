908e5464ef2466111374091679005744bf0f5e35
Dominic Padula <domopadula2192@gmail.com>
2021-01-18 09:42:01 -0700

    Tests that show encrpt is working

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 require './lib/enigma'
 require './lib/keygenerator'
+require './lib/rotate'
 
 
 class EnigmaTest < Minitest::Test
@@ -12,3 +13,11 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_encrypt_is_hash_taking_three_arguements
+    expected = {encryption: 'keder ohulw!',
+                key: '02715',
+                date: '040895'}
+    assert_equal expected, @enigma.encrypt('Hello World!', '02715', '040895')
+  end
+end
