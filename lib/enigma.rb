4e2d1ec4912de1ba3b744aee9ff55dc56ab6c89b
Dominic Padula <domopadula2192@gmail.com>
2021-01-18 08:11:55 -0700

    Write CLI need to refactor

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,51 +1,18 @@
-class Enigma
-  def initialize
-  end
+require './lib/enigma'
 
-  def alphabet
-    ("a".."z").to_a << " "
-  end
+message = File.read(ARGV[0], 'r')
+text = message.read
+message.close
 
-  def convert_date
-    Date.today.strftime("%m%d%y")
-  end
+enigma = Enigma.new
 
-  def key_numbers
-    5.times.map do |num|
-        rand(9)
-    end.join
-  end
-
-  def split_keys(digits)
-   digits.split('').each_cons(2).map do |pair|
-      pair.join.to_i
-    end
-  end
-
-  def create_offsets(convert_date)
-    hold = []
-    x = (convert_date.to_i)
-    y = x ** 2
-    a = y.to_s[-4..-1]
-    a.split('')
-    hold << a.split('').map(&:to_i)
-    hold.flatten
-  end
-
-  def create_values(arg_1, arg_2)
-    split = split_keys(arg_1)
-    offset = create_offsets(arg_2)
-    combine = [offset, split]
-    combine.transpose.map {|x| x.reduce(:+)}
-  end
-
-  def final_shifts(values)
-    hash = {A:values[0],B:values[1],C:values[2],D:values[3]}
-  end
-
-  # def encrypt(message, key = default_key, date = default_date)
-  #   {encryption: cipher,
-  #               key: key,
-  #               date: date}
-  # end
+encoded = enigma.encrypt(text.chomp)
+encrpted_file = File.open(ARGV[1], 'w')
+encrpted_file.write(encoded[:encrption])
+encrpted_file.close
+puts "Created #{ARGV[1]} with the key #{encoded[:key]} and the date #{encoded[:date]} "
+def encrypt(message, key = default_key, date = convert_date)
+  {encryption: cipher,
+          key: key,
+         date: date}
 end
