9ebf93820f89c823438046630ba1307fac97c9e2
Dominic Padula <domopadula2192@gmail.com>
2021-01-18 13:46:26 -0700

    refactor for testing

--- lib/encrpt.rb
+++ lib/encrpt.rb
@@ -4,7 +4,7 @@ message = File.read(ARGV[0])
 
 
 enigma = Enigma.new
-encoded = enigma.encrypt(message)
+encoded = enigma.encrypt(message.downcase)
 
 encrypted_file = File.open(ARGV[1], 'w').write(encoded[:encryption])
 
